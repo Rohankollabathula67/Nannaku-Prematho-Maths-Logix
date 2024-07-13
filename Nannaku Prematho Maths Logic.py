ad58a0254fcc602e67641b5fb083c1ee969805ef
Rohan <118647297+Rohankollabathula67@users.noreply.github.com>
2024-07-13 07:47:00 +0530

    Add files via upload

---
+++ Nannaku Prematho Maths Logic.py
@@ -0,0 +1,15 @@
+def nth_person_alive(n):
+    powers_of_2 = [ 2 ** i for i in range(1,51)]
+    if n == 1 :
+        return 1
+    for j in range(n):
+        if  powers_of_2[j] > n :
+            toNegative = powers_of_2[j-1]
+            break
+    return (n - toNegative)*2 + 1
+    print(toNegative)
+        
+n = 100
+result = nth_person_alive(n)
+
+print(f"{result} Person will alive")
